739235b80c06212a15913280aac78bab98943c8d
Josh Montoute <josh@thinksquirrel.com>
2014-09-13 00:38:19 -0500

    Full documentation

--- 1.4.0f1/reference/search/all_d.js
+++ 1.4.0f1/reference/search/all_d.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['uishakemodifier',['uiShakeModifier',['../class_thinksquirrel_1_1_utilities_1_1_camera_shake.html#a3240c69bac8f64e7ab520ddb0ecc8a8b',1,'Thinksquirrel::Utilities::CameraShake']]],
-  ['uishakerect',['uiShakeRect',['../class_thinksquirrel_1_1_utilities_1_1_camera_shake.html#ab179ef3686be391110940dd79ef6c57c',1,'Thinksquirrel::Utilities::CameraShake']]],
-  ['userdata',['userData',['../class_thinksquirrel_1_1_utilities_1_1_camera_shake_base.html#a60e040fc2de55749cb3510845ee15f62',1,'Thinksquirrel::Utilities::CameraShakeBase']]]
+  ['uishakerect',['uiShakeRect',['../class_thinksquirrel_1_1_utilities_1_1_camera_shake.html#ab179ef3686be391110940dd79ef6c57c',1,'Thinksquirrel::Utilities::CameraShake']]]
 ];
